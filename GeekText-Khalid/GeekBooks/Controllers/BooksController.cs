992eefdb1b5e4c5221eb0911fb785d3547a1d7ce
Kalamoudi <46762886+Kalamoudi@users.noreply.github.com>
2019-06-24 20:40:24 -0400

    Commited

---
+++ GeekText-Khalid/GeekBooks/Controllers/BooksController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GeekBooks.DAL;
+
+namespace GeekBooks.Controllers
+{
+    public class BooksController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: Books
+        public ActionResult Index(string movieGenre, string searchString)
+        {
+
+            var GenreList = new List<string>();
+
+           // var GenreLst = from d in db.Genres
+                 //          select d.GenreName;
+
+            var GenreQry = from d in db.Genres
+                           orderby d.GenreName
+                           select d.GenreName;
+
+            GenreList.AddRange(GenreQry.Distinct());
+            
+            ViewBag.movieGenre = new SelectList(GenreList);
+
+            var books = from m in db.Books
+                        select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                books = books.Where(s => s.Title.Contains(searchString));
+            }
+
+            if (!String.IsNullOrEmpty(movieGenre))
+            {
+                books = books.Where(x => x.GenreName == movieGenre);
+            }
+
+            return View(books);
+        }
+
+        // GET: Books/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Book book = db.Books.Find(id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+
+       public ActionResult ShoppingCart(int? id)//id + userID i assume
+        {
+            //Code to replace, just added to compile
+            Book book = db.Books.Find(id);
+            return View(book);
+        }
+
+        public ActionResult WishList(int? id)//id + userID i assume)
+        {
+            //Code to replace, just added to compile
+            Book book = db.Books.Find(id);
+            return View(book);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ GeekText-Khalid/GeekBooks/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GeekBooks")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GeekBooks")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("30431fc1-527f-46c1-be2a-ed6620399740")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
